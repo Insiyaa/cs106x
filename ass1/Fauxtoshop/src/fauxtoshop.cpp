f84320713bb2395f7b6ade426abd6d807141b6b7
Insiyaa <insiyahhajoori@gmail.com>
2018-12-30 20:49:49 +0530

    Organise and add G blur

--- ass1/Fauxtoshop/src/fauxtoshop.cpp
+++ ass1/Fauxtoshop/src/fauxtoshop.cpp
@@ -1,7 +1,3 @@
-// This is the CPP file you will edit and turn in.
-// Also remove these comments here and add your own.
-// TODO: rewrite this comment
-
 #include <iostream>
 #include "console.h"
 #include "gwindow.h"
@@ -10,8 +6,9 @@
 #include "strlib.h"
 #include "gbufferedimage.h"
 #include "gevents.h"
-#include "math.h" //for sqrt and exp in the optional Gaussian kernel
-#include "gmath.h" // for sinDegrees(), cosDegrees(), PI
+#include "math.h"
+#include "gmath.h"
+
 using namespace std;
 
 static const int    WHITE = 0xFFFFFF;
@@ -26,13 +23,114 @@ void     getMouseClickLocation(int &row, int &col);
 Vector<double> gaussKernelForRadius(int radius);
 
 
-void Scatter(GBufferedImage& img, const Grid<int>& img_grid, int degree) {
-    // create dup of grid and mutate img reference.
+void Scatter(GBufferedImage& img, const Grid<int>& img_grid);
+bool isEdge(const Grid<int>& img_grid, int threshold, int r, int c);
+void EdgeDetect(GBufferedImage& img,const Grid<int>& img_grid);
+bool isGreen(const Grid<int> stk_grid, int row, int col, int tol);
+void GreenScreen(GBufferedImage& img, const Grid<int>& img_grid);
+void Compare(GBufferedImage& img);
+void Rotate(GBufferedImage& img, const Grid<int>& img_grid);
+void GaussianBlur(GBufferedImage& img, const Grid<int>& img_grid);
 
-    Grid<int> duplicate = img_grid;
+/*
+ * This main simply declares a GWindow and a GBufferedImage for use
+ * throughout the program. By asking you not to edit this function,
+ * we are enforcing that the GWindow have a lifespan that spans the
+ * entire duration of execution (trying to have more than one GWindow,
+ * and/or GWindow(s) that go in and out of scope, can cause program
+ * crashes).
+ */
+int main() {
+    GWindow gw;
+    gw.setTitle("Fauxtoshop");
+    gw.setVisible(true);
+    GBufferedImage img;
+    doFauxtoshop(gw, img);
+    return 0;
+}
+
+void doFauxtoshop(GWindow &gw, GBufferedImage &img) {
+
+    cout << "Welcome to Fauxtoshop!" << endl;
+    openImageFromFilename(img, "kitten.jpg");
+    gw.setCanvasSize(img.getWidth(), img.getHeight());
+    gw.add(&img,0,0);
+
+    while(true) {
+        string filename = getLine("Enter the name of file to open(blank to quit):");
+        if (filename == ""){
+            break;
+        }
+        cout << "Opening image, make take a while..." << endl;
+        bool status = openImageFromFilename(img, filename);
+        if (!status) {
+            cout << "Image doesn't exist, try again!" << endl;
+        }
+        else {
+            Grid<int> image_grid = img.toGrid();
+
+            gw.setCanvasSize(img.getWidth(), img.getHeight());
+            gw.add(&img,0,0);
+
+            cout << "Which image filter would you like to apply?" << endl;
+            cout << "1 - Scatter" << endl;
+            cout << "2 - Edge detection" << endl;
+            cout << "3 - Green screen with another image" << endl;
+            cout << "4 - Compare with another image" << endl;
+            cout << "5 - Rotation" << endl;
+            cout << "6 - Gaussian blur" << endl;
+
+            int choice;
+            do {
+                choice = getInteger("Your choice: ");
+            }while(choice < 1 || choice > 6);
+
+            switch (choice) {
+            case 1:               
+                Scatter(img, image_grid);
+                break;
+            case 2:
+                EdgeDetect(img, image_grid);
+                break;
+            case 3:
+                GreenScreen(img, image_grid);
+                break;
+            case 4:
+                Compare(img);
+                break;
+            case 5:
+                Rotate (img, image_grid);
+                break;
+            case 6:
+                GaussianBlur(img, image_grid);
+                break;
+            default:
+                cout << "Enter valid value." << endl;
+                break;
+            }
+
+            string imagesave = getLine("Enter filename to save image or blank to skip: ");
+            if (!(imagesave == "")) {
+                saveImageToFilename(img, imagesave);
+            }
+        }
+
+    }
+    gw.clear();
+}
+
+void Scatter(GBufferedImage& img, const Grid<int>& img_grid) {
+
+    Grid<int> duplicate = img.toGrid();
 
     int rows = img_grid.numRows();
     int cols = img_grid.numCols();
+
+    int degree;
+    do {
+        degree = getInteger("Enter degree of scatter (1-100): ");
+    } while (degree > 100 || degree < 1);
+
 
     for (int r = 0; r < rows; r++) {
         for (int c = 0; c < cols; c++) {
@@ -40,7 +138,7 @@ void Scatter(GBufferedImage& img, const Grid<int>& img_grid, int degree) {
             do {
                 ran_r = randomInteger(-degree, degree);
                 ran_c = randomInteger(-degree, degree);
-            } while(r+ran_r < 0 || r+ran_r >= rows || c+ran_c < 0 || c+ran_c >= cols);
+            } while(!img_grid.inBounds(r+ran_r, c+ran_c));
 
             duplicate[r][c] = img_grid[r+ran_r][c+ran_c];
 
@@ -50,7 +148,7 @@ void Scatter(GBufferedImage& img, const Grid<int>& img_grid, int degree) {
     img.fromGrid(duplicate);
 }
 
-bool isEdge(GBufferedImage& img, const Grid<int>& img_grid, int threshold, int r, int c) {
+bool isEdge(const Grid<int>& img_grid, int threshold, int r, int c) {
 
     int r1, g1, b1, r2, g2, b2;
 
@@ -69,14 +167,19 @@ bool isEdge(GBufferedImage& img, const Grid<int>& img_grid, int threshold, int r
     return false;
 }
 
-void EdgeDetect(GBufferedImage& img,const Grid<int>& img_grid, int threshold) {
+void EdgeDetect(GBufferedImage& img,const Grid<int>& img_grid) {
     Grid<int> duplicate =  img_grid;
     int rows = img_grid.numRows();
     int cols = img_grid.numCols();
 
+    int threshold;
+    do {
+        threshold = getInteger("Enter threshold for edge detection (>0): ");
+    } while (threshold < 1);
+
     for (int r = 0; r < rows; r++) {
         for (int c = 0; c < cols; c++) {
-            if (isEdge(img, img_grid, threshold, r, c)) {
+            if (isEdge(img_grid, threshold, r, c)) {
                 duplicate[r][c] = BLACK;
             }
             else {
@@ -90,8 +193,10 @@ void EdgeDetect(GBufferedImage& img,const Grid<int>& img_grid, int threshold) {
 }
 
 bool isGreen(const Grid<int> stk_grid, int row, int col, int tol) {
+
     int r1, g1, b1, r2, g2, b2;
     GBufferedImage::getRedGreenBlue(GREEN, r1, g1, b1);
+
     GBufferedImage::getRedGreenBlue(stk_grid[row][col], r2, g2, b2);
 
     if (max(abs(r1-r2), max(abs(g1-g2), abs(b1-b2))) < tol) {
@@ -102,10 +207,47 @@ bool isGreen(const Grid<int> stk_grid, int row, int col, int tol) {
     }
 }
 
-void GreenScreen(GBufferedImage& img, const Grid<int>& img_grid, const Grid<int> stk_grid, int row, int col, int tol) {
+void GreenScreen(GBufferedImage& img, const Grid<int>& img_grid) {
     Grid<int> duplicate = img_grid;
+    GBufferedImage sticker;
+
+    // 1. Prompt for sticker image
+    while (true) {
+        string filename = getLine("Enter the name of file to superimpose: ");
+
+        bool stat = openImageFromFilename(sticker, filename);
+        if (!stat) {
+            cout << "Image doesn't exist, try again!" << endl;
+        }
+        else {
+            break;
+        }
+    }
+
+    Grid<int> stk_grid = sticker.toGrid();
     int rows = stk_grid.numRows();
     int cols = stk_grid.numCols();
+
+    // 2. Prompt for tolerance for pure green (1 - 100)
+    int tol;
+    do {
+        tol = getInteger("Enter tolerance (1-100): ");
+    } while (tol > 100 || tol < 1);
+
+    // 3. Location of sticker. Non-negative "(row, col)". Place upper left corner of sticker. Or no input and mouse click.
+    int row, col;
+    char a;
+    do {
+        string loc = getLine("Enter location to place the sticker img as \"(row,col)\" (or blank to use mouse):");
+    if (loc != "") {
+        stringstream ss(loc);
+        ss >> a >> row >> a >> col;
+    }
+    else {
+        getMouseClickLocation(row, col);
+    }
+        cout << endl << "Get location: (" << row << ", " << col << ")." << endl;
+    } while (row < 0 || col < 0);
 
     for (int r = 0; r < rows; r++) {
         for (int c = 0; c < cols; c++) {
@@ -119,8 +261,36 @@ void GreenScreen(GBufferedImage& img, const Grid<int>& img_grid, const Grid<int>
     img.fromGrid(duplicate);
 }
 
-void Rotate(GBufferedImage& img, const Grid<int>& img_grid, int angle) {
-    // create dup of grid and mutate img reference.
+void Compare(GBufferedImage& img) {
+    GBufferedImage img2;
+    cout << "Choose another image file to compare with." << endl;
+
+    while (true) {
+        string filename = getLine("Enter filename: ");
+
+        bool stat = openImageFromFilename(img2, filename);
+        if (!stat) {
+            cout << "Image doesn't exist, try again!" << endl;
+        }
+        else {
+            break;
+        }
+    }
+
+    int count = img.countDiffPixels(img2);
+    if (count == 0) {
+        cout << "The images are the same." << endl;
+    } else {
+        cout << "The images differ by " << count << " pixels." << endl;
+    }
+}
+
+void Rotate(GBufferedImage& img, const Grid<int>& img_grid) {
+
+    int angle;
+    do {
+        angle = getInteger("Please enter an angle in degrees(0 - 360):  ");
+    } while (angle > 360 || angle < 0);
 
     Grid<int> duplicate = img_grid;
 
@@ -145,231 +315,76 @@ void Rotate(GBufferedImage& img, const Grid<int>& img_grid, int angle) {
 
     img.fromGrid(duplicate);
 }
-void GaussianBlur(GBufferedImage& img, const Grid<int>& img_grid, const Vector<double>& kernel) {
+void GaussianBlur(GBufferedImage& img, const Grid<int>& img_grid) {
     // create dup of grid and mutate img reference.
 
-    Grid<int> duplicate = img_grid;
+    int radius;
+    do {
+        radius = getInteger("Enter radius greater than 0: ");
+    } while (radius < 0);
 
-    int rows = img_grid.numRows();
-    int cols = img_grid.numCols();
+    Vector<double> kernel = gaussKernelForRadius(radius);
 
-    int len = kernel.size();
+    int rows = img_grid.height();
+    int cols = img_grid.width();
 
-    for (int r = 0; r < rows; r++) {
-        for (int c = 0; c < cols; c++) {
-            for (int k = 0; k < len; k++) {
-                if(img_grid.inBounds(r+k, c)) {
-                    duplicate [r][c] += img_grid[r+k][c] * kernel[k];
-                }
+    Grid<int> grid1 = img.toGrid();
+    Grid <int> grid2 (rows, cols);
+
+    // Horizontal
+    for (int i = 0; i < cols; i++) {
+        for (int j = 0; j < rows; j++) {
+            double r = 0, g = 0, b = 0;
+
+            for (int k = -radius; k <= radius; k++) {
+                int r1, g1, b1;
+
+                if (i + k < 0)
+                    GBufferedImage::getRedGreenBlue(grid1[j][0], r1, g1, b1);
+                else if (i + k >= cols)
+                    GBufferedImage::getRedGreenBlue(grid1[j][cols - 1], r1, g1, b1);
+                else
+                    GBufferedImage::getRedGreenBlue(grid1[j][i + k], r1, g1, b1);
+
+                r += kernel[k + radius] * r1;
+                g += kernel[k + radius] * g1;
+                b += kernel[k + radius] * b1;
             }
 
+            grid2[j][i] = GBufferedImage::createRgbPixel(r, g, b);
         }
     }
 
-    img.fromGrid(duplicate);
-}
+    // Vertical
+    for (int i = 0; i < cols; i++) {
+        for (int j = 0; j < rows; j++) {
+            double r = 0, g = 0, b = 0;
 
+            for (int k = -radius; k <= radius; k++) {
+                int r1, g1, b1;
 
-/* STARTER CODE FUNCTION - DO NOT EDIT
- *
- * This main simply declares a GWindow and a GBufferedImage for use
- * throughout the program. By asking you not to edit this function,
- * we are enforcing that the GWindow have a lifespan that spans the
- * entire duration of execution (trying to have more than one GWindow,
- * and/or GWindow(s) that go in and out of scope, can cause program
- * crashes).
- */
-int main() {
-    GWindow gw;
-    gw.setTitle("Fauxtoshop");
-    gw.setVisible(true);
-    GBufferedImage img;
-    doFauxtoshop(gw, img);
-    return 0;
-}
+                if (j + k < 0)
+                    GBufferedImage::getRedGreenBlue(grid2[0][i], r1, g1, b1);
+                else if (j + k >= rows)
+                    GBufferedImage::getRedGreenBlue(grid2[rows - 1][i], r1, g1, b1);
+                else
+                    GBufferedImage::getRedGreenBlue(grid2[j + k][i], r1, g1, b1);
 
-/* This is yours to edit. Depending on how you approach your problem
- * decomposition, you will want to rewrite some of these lines, move
- * them inside loops, or move them inside helper functions, etc.
- *
- * TODO: rewrite this comment.
- */
-void doFauxtoshop(GWindow &gw, GBufferedImage &img) {
+                r += kernel[k + radius] * r1;
+                g += kernel[k + radius] * g1;
+                b += kernel[k + radius] * b1;
+            }
 
-    cout << "Welcome to Fauxtoshop!" << endl;
-    openImageFromFilename(img, "kitten.jpg");
-    gw.setCanvasSize(img.getWidth(), img.getHeight());
-    gw.add(&img,0,0);
-
-    while(true) {
-        string filename = getLine("Enter the name of file to open(blank to quit):");
-        if (filename == ""){
-            break;
+            grid1[j][i] = GBufferedImage::createRgbPixel(r, g, b);
         }
-        cout << "Opening image, make take a while..." << endl;
-        bool status = openImageFromFilename(img, filename);
-        if (!status) {
-            cout << "Image doesn't exist, try again!" << endl;
-        }
-        else {
-            Grid<int> image_grid = img.toGrid();
-            gw.setCanvasSize(img.getWidth(), img.getHeight());
-            gw.add(&img,0,0);
-
-
-
-            cout << "Which image filter would you like to apply?" << endl;
-            cout << "1 - Scatter" << endl;
-            cout << "2 - Edge detection" << endl;
-            cout << "3 - Green screen with another image" << endl;
-            cout << "4 - Compare with another image" << endl;
-            cout << "5 - Rotation" << endl;
-            cout << "6 - Gaussian blur" << endl;
-
-            int choice;
-            do {
-                choice = getInteger("Your choice: ");
-            }while(choice < 0 || choice > 6);
-
-            switch (choice) {
-            case 1:
-                // Scatter
-                int degree;
-                do {
-                    degree = getInteger("Enter degree of scatter (1-100): ");
-                } while (degree > 100 || degree < 1);
-
-                Scatter(img, image_grid, degree);
-                break;
-            case 2:
-
-                //Edge Detection
-
-                int threshold;
-                do {
-                    threshold = getInteger("Enter degree of scatter (1-100): ");
-                } while (threshold < 1);
-
-                EdgeDetect(img, image_grid, threshold);
-
-                break;
-            case 3:
-            {
-                // Green Screen
-                GBufferedImage sticker;
-
-                // 1. Prompt for sticker image
-                while (true) {
-                    string filename = getLine("Enter the name of file to superimpose: ");
-
-                    bool stat = openImageFromFilename(sticker, filename);
-                    if (!stat) {
-                        cout << "Image doesn't exist, try again!" << endl;
-                    }
-                    else {
-                        break;
-                    }
-                }
-
-                Grid<int> stk_grid = sticker.toGrid();
-
-                // 2. Prompt for tolerance for pure green (1 - 100)
-                int tol;
-                do {
-                    tol = getInteger("Enter tolerance (1-100): ");
-                } while (tol > 100 || tol < 1);
-
-                // 3. Location of sticker. Non-negative "(row, col)". Place upper left corner of sticker. Or no input and mouse click.
-                int row, col;
-                char a;
-                do {
-                    string loc = getLine("Enter location to place the sticker img as \"(row,col)\" (or blank to use mouse):");
-                if (loc != "") {
-                    stringstream ss(loc);
-                    ss >> a >> row >> a >> col;
-                }
-                else {
-                    getMouseClickLocation(row, col);
-                }
-                    cout << endl << "Get location: (" << row << ", " << col << ")." << endl;
-                } while (row < 0 || col < 0);
-
-                // 4. Place sticker. Copy onto background if green value greater than threshold.
-                GreenScreen(img, image_grid, stk_grid, row, col, tol);
-
-                break;
-            }
-            case 4:
-            {
-                GBufferedImage img2;
-                cout << "Now choose another image file to compare with." << endl;
-
-                while (true) {
-                    string filename = getLine("Enter the name of file to superimpose: ");
-
-                    bool stat = openImageFromFilename(img2, filename);
-                    if (!stat) {
-                        cout << "Image doesn't exist, try again!" << endl;
-                    }
-                    else {
-                        break;
-                    }
-                }
-
-                int count = img.countDiffPixels(img2);
-                if (count == 0) {
-                    cout << "The images are the same." << endl;
-                } else {
-                    cout << "The images differ by " << count << " pixels." << endl;
-                }
-
-                break;
-            }
-
-            case 5:
-            {
-                int angle;
-                do {
-                    angle = getInteger("Please enter an angle in degrees(0 - 360):  ");
-                } while (angle > 360 || angle < 0);
-
-                Rotate (img, image_grid, angle);
-
-                break;
-            }
-            case 6:
-            {
-                int radius;
-                do {
-                    radius = getInteger("Enter radius greater than 0: ");
-                } while (radius < 0);
-
-                Vector<double> kernel = gaussKernelForRadius(radius);
-
-                GaussianBlur(img, image_grid, kernel);
-
-                break;
-            }
-
-
-            default:
-                cout << "Enter valid value." << endl;
-                break;
-            }
-
-            string imagesave = getLine("Enter filename to save image or blank to skip: ");
-            if (!(imagesave == "")) {
-                saveImageToFilename(img, imagesave);
-            }
-        }
-
     }
-    gw.clear();
+
+
+    img.fromGrid(grid1);
 }
 
 
-/* STARTER CODE HELPER FUNCTION - DO NOT EDIT
- *
+/*
  * Attempts to open the image file 'filename'.
  *
  * This function returns true when the image file was successfully
@@ -382,8 +397,7 @@ bool openImageFromFilename(GBufferedImage& img, string filename) {
     return true;
 }
 
-/* STARTER CODE HELPER FUNCTION - DO NOT EDIT
- *
+/*
  * Attempts to save the image file to 'filename'.
  *
  * This function returns true when the image was successfully saved
@@ -395,8 +409,7 @@ bool saveImageToFilename(const GBufferedImage &img, string filename) {
     return true;
 }
 
-/* STARTER CODE HELPER FUNCTION - DO NOT EDIT
- *
+/*
  * Waits for a mouse click in the GWindow and reports click location.
  *
  * When this function returns, row and col are set to the row and
@@ -411,8 +424,7 @@ void getMouseClickLocation(int &row, int &col) {
     col = me.getX();
 }
 
-/* HELPER FUNCTION
- *
+/*
  * This is a helper function for the Gaussian blur option.
  *
  * The function takes a radius and computes a 1-dimensional Gaussian blur kernel
